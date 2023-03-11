ae6c8085c3b0877990e50d40fd8cd8b5580b4578
Yuandjom <ziyangjohn@gmail.com>
2023-03-11 15:28:20 +0800

    sorted post

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -29,7 +29,7 @@ export const createPost = async (req, res) => {
 /* READ */
 export const getFeedPosts = async (req, res) => {
   try {
-    const post = await Post.find();
+    const post = await Post.find().sort({ _id: -1 });
     res.status(200).json(post);
   } catch (err) {
     res.status(404).json({ message: err.message });
@@ -39,7 +39,7 @@ export const getFeedPosts = async (req, res) => {
 export const getUserPosts = async (req, res) => {
   try {
     const { userId } = req.params;
-    const post = await Post.find({ userId });
+    const post = await Post.find({ userId }).sort({ _id: -1 });
     res.status(200).json(post);
   } catch (err) {
     res.status(404).json({ message: err.message });
