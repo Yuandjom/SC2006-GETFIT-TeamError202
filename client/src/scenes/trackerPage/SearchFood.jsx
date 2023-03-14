8cb04377ed40ba186c467a355696e4050da0d696
JingSiong <yujingsiong@gmail.com>
2023-03-14 22:08:44 +0800

    added food popup

--- client/src/scenes/trackerPage/SearchFood.jsx
+++ client/src/scenes/trackerPage/SearchFood.jsx
@@ -15,14 +15,24 @@ export default function SearchFood() {
     }
 
     const handleSubmitFood = (food) => {
+        /*
+        const foodInfo = {
+            name: food.name,
+            protein: food.protein,
+            lipids: food.lipids,
+            carbohydrates: food.carbohydrates,
+        }
+        */
         setChosenFood(food);
     }
 
+    
+
     //call the api
-    const Search = (e) => {
-        e.preventDefault();
+    const Search = () => {
 
         setFoodData([]);
+        setChosenFood({});
 
         //call API
         const {REACT_APP_FOOD_API_KEY} = process.env;
@@ -66,48 +76,50 @@ export default function SearchFood() {
                 value = {input}
                 onChange = {handleChange}
                 />
-                <button onClick={Search}>
+                <button onClick={() => Search()}>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
                     </svg>
                 </button>
             </div>
-            <div> 
-                {foodData.length > 0 && 
-                <div className="ml-12 overflow-y-scroll h-72 w-4/5 md:w-3/5 lg:w-2/5 border border-black px-5">
-                    <div className="mt-4 grid grid-cols-4 gap-2 font-bold">
-                            <h2> Description </h2>
-                            <h2> Protein </h2>
-                            <h2> Lipids </h2>
-                            <h2> Carbohydrates </h2>
-                    </div>
-                    <hr className="my-2 border-1 border-gray-300"></hr>
-                    
-                    {foodData.map(food => (
-                    <div>
-                        <div className="mt-4 grid grid-cols-4 items-center">
-                            <h2> {food.name} </h2>
-                            <h2> {food.protein} </h2>
-                            <h2> {food.lipids} </h2>
-                            <div className="flex justify-between items-center"> 
-                                <h2> {food.carbohydrates} </h2>
-                                <button onClick={handleSubmitFood(food)}>
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                                        <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
-                                    </svg>
-                                </button>
-                            </div>
+            <div className="grid grid-cols-1 lg:grid-cols-2 gap-x-0"> 
+                <div> 
+                    {foodData.length > 0 && 
+                    <div className="ml-12 mb-10 overflow-y-scroll h-72 w-4/5 md:w-3/5 lg:w-4/5 border border-black px-5">
+                        <div className="mt-4 grid grid-cols-4 gap-2 font-bold">
+                                <h2> Description </h2>
+                                <h2> Protein </h2>
+                                <h2> Lipids </h2>
+                                <h2> Carbohydrates </h2>
                         </div>
                         <hr className="my-2 border-1 border-gray-300"></hr>
-                    </div>
-                    ))}
-                </div>}
-
-                { Object.values(chosenFood).length == 0 &&
-                <SubmitFood 
-                food= {chosenFood} />
-                }  
-
+                        
+                        {foodData.map(food => (
+                        <div>
+                            <div className="mt-4 grid grid-cols-4 items-center">
+                                <h2> {food.name} </h2>
+                                <h2> {food.protein} </h2>
+                                <h2> {food.lipids} </h2>
+                                <div className="flex justify-between items-center"> 
+                                    <h2> {food.carbohydrates} </h2>
+                                    <button onClick={() => handleSubmitFood(food)}>
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                            <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+                                        </svg>
+                                    </button>
+                                </div>
+                            </div>
+                            <hr className="my-2 border-1 border-gray-300"></hr>
+                        </div>
+                        ))}
+                    </div>}
+                </div>
+                <div>
+                    { Object.values(chosenFood).length !== 0 &&
+                    <SubmitFood 
+                    food= {chosenFood} />
+                    }  
+                </div>
             </div>
         </div>
     )
--- client/src/scenes/trackerPage/trackerComponents/SubmitFood.jsx
+++ client/src/scenes/trackerPage/trackerComponents/SubmitFood.jsx
@@ -1,12 +1,55 @@
+import { useState } from "react";
 
 
 export default function SubmitFood({food}) {
-
-    console.log(food);
+    
+    const [servings,setServings] = useState(0);
+    const [meal, setMeal] = useState('Breakfast');
 
     return (
-        <div>
-            <h1> Submit Food </h1>
+        <div className="ml-12 mb-10 bg-white h-72 w-4/5 md:w-3/5 lg:w-4/5 border border-black p-5 flex flex-col items-center rounded-xl">
+            
+            <h2 className="text-xl font-bold"> {food.name} </h2>
+            <div className="grid grid-cols-2 gap-5">
+                <div className="flex flex-col items-center">
+                    <h2 className="mt-3 text-xl"> Serving portion </h2>
+
+                    <div className="mt-6 flex gap-3 items-center">
+                        <input 
+                        className="px-4 w-16 h-10 shadow shadow-inner shadow-xl shadow-black p-3"
+                        type="number" 
+                        placeholder="1.0"
+                        value = {servings}
+                        onChange = {e => setServings(e.target.value)}
+                        />
+                        <h2> servings of </h2>
+                        <h2> 1 cup </h2>
+                    </div>
+
+                    <h2 className="mt-5 text-xl"> Which meal? </h2>
+
+                    <div className="mt-5">
+                        <label className="shadow shadow-inner shadow-xl shadow-black p-3">
+                            <select name="selectedFruit" value={meal} onChange={e => setMeal(e.target.value)}>
+                                <option value="Breakfast">Breakfast</option>
+                                <option value="Lunch">Lunch</option>
+                                <option value="Dinner">Dinner</option>
+                            </select>
+                        </label>
+                    </div>
+                </div>
+                <div className="px-5">
+                    <h2 className="mt-3 text-xl text-center"> Nutrients: </h2> 
+                    <div className="mt-3">               
+                        <h2> Protein: {food.protein} </h2>
+                        <h2> Lipids (Fats):  {food.lipids} </h2>
+                        <h2> Carbohydrates: {food.carbohydrates} </h2>
+                    </div> 
+                    <div className="flex justify-center mt-6">  
+                        <button className="bg-gray-400 p-3 rounded-xl text-white text-lg"> Add Food </button>
+                    </div> 
+                </div>
+            </div>
             
         </div>
     )
