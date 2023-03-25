77965c3206fb905a88e58b78ac846ff18b804d43
EsshIsMe <116577415+EsshIsMe@users.noreply.github.com>
2023-03-25 11:19:22 +0800

    added searchbar

--- client/src/scenes/searchPage/Search.jsx
+++ client/src/scenes/searchPage/Search.jsx
@@ -7,6 +7,9 @@ import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import PostsWidget from "scenes/widgets/PostsWidget";
 import UserWidget from "scenes/widgets/UserWidget";
+import "./List.css";
+import { Users } from "./users";
+import Table from "./Table";
 // import List from './List';
 
 function Search() {
@@ -15,6 +18,13 @@ function Search() {
     const { _id, picturePath } = useSelector((state) => state.user);
     const token = useSelector((state) => state.token);
     const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
+   const [query, setQuery] = useState("");
+   const keys = ["first_name", "last_name", "email"];
+   const search = (data) => {
+     return data.filter((item) =>
+       keys.some((key) => item[key].toLowerCase().includes(query))
+     );
+   };
 
     const getUser = async () => {
         const response = await fetch(`http://localhost:3001/users/${_id}`, {
@@ -31,7 +41,8 @@ function Search() {
     
       if (!user) return null;
     
-  return (
+    /*logic for search bar outside return*/
+    return (
     <Box>
       <Navbar />
       <Box
@@ -52,7 +63,10 @@ function Search() {
         >
           <Box justifyContent={'center'}>
             <Typography fontSize={20}> Find Friends </Typography>
+            {/*return statement code for search bar here*/}
+            <input type="text" placeholder='Search...' className='search' onChange={(e)=> setQuery(e.target.value.toLowerCase())} />
             {/*<List />*/}
+            <Table data={search(Users)}/>
           </Box>
           <Box m="2rem 0" />
           {/* <PostsWidget userId={userId} isProfile /> */}
