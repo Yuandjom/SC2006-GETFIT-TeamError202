3dcdcaf4701bd08de60f68ba089125b34f087a61
Ken Ho <96031026+kenho01@users.noreply.github.com>
2023-03-25 12:03:54 +0800

    Revert "Revert "Revert "Merge branch 'master' of https://github.com/Yuandjom/SC2006-GETFIT-TeamError202"""
    This reverts commit 02cdac1da08b4c2f900908f17837df6b80ac25e0.

--- client/src/scenes/searchPage/List.jsx
+++ client/src/scenes/searchPage/List.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import TextField from "@mui/material/TextField";
 import data from "../../components/List";
-import "./List.css";
 
 export default function List() {
 
